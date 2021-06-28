1705a960a83c6fa7ea4dc7c88a364b42232511cd
shubham7090 <shubham.70@gmail.com>
2021-06-28 09:24:54 +0530

    Renamed the files to make more sense.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-
-const menuToggle = document.querySelector('.toggle');
-const menuIcon=document.querySelector('.toggle i')
-const menuShowcase = document.querySelector('.showcase');
-menuToggle.addEventListener("click",function(){
-    menuShowcase.classList.toggle('active');
-    if(menuIcon.classList.contains("fas")){
-        console.log("yes");
-        menuIcon.classList.remove("fas","fa-bars");
-        menuIcon.classList.add("far","fa-times-circle");
-    }else{
-        menuIcon.classList.remove("far","fa-times-circle");
-        menuIcon.classList.add("fas","fa-bars");
-    }
+const time = document.querySelector("time");
+document.querySelector(".glass").addEventListener("click",function(){
+    window.location.href="brickwall.html";
 });
 
-const portKey= document.querySelector(".text a");
-portKey.addEventListener("click",function(){
-    portKey.innerHTML="PortKey is outside your Window";
-});
+function DisplayTime() {
+    let today = new Date();
+    let hour = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+    const ampm = hour >= 12 ? "PM" : "AM";
+    hour = hour % 12 == 0 ? 12 : hour % 12;
+
+    time.innerHTML = `${addZero(hour)} : ${addZero(min)} : ${addZero(sec)} ${ampm}`;
+
+    setTimeout(DisplayTime,1000);
+
+}
+function addZero(n){
+    if(n<9)return "0"+n;
+    else return n;
+}
+DisplayTime();
